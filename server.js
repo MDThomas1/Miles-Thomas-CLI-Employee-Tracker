d1103f1b7c9109d15a2a77fcbd424b0249541358
Miles Thomas <milesman2013@gmail.com>
2022-11-01 18:15:02 +1100

    opening prompt and ending app function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
 const express = require('express')
+const { default: Choices } = require('inquirer/lib/objects/choices')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -18,6 +19,54 @@ const db = mysql.createConnection(
 function beginApp() {
     inquirer
     .prompt([
-        {}
+        {
+            name: 'selections',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View all roles',
+                'Add a role',
+                'View all departments',
+                'Add a department',
+                'View all employees',
+                'Add an employee',
+                'Update an employee role',
+                'Exit the app',
+            ]
+        }
+        .then((answers) => {
+            switch (answers.selections) {
+                case 'View all roles':
+                    viewRoles()
+                    break;
+                case 'Add a role':
+                    addRole()
+                    break;
+                case 'View all departments':
+                    viewDepartments()
+                    break;
+                case 'Add a department':
+                    addDepertment()
+                    break;
+                case 'View all employees':
+                    viewEmployees()
+                    break;
+                case 'Add an employee':
+                    addEmployee()
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole()
+                default:
+                    endApp()
+                    break;
+            }
+        })
     ])
+
+    function endApp() {
+        console.log('Exiting application. Thank you!')
+        process.exit()
+    }
 }
+
+beginApp()
