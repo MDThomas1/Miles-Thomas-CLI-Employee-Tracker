9dd742841286337c42ff9411b63ee4a2f51d77a7
Miles Thomas <milesman2013@gmail.com>
2022-11-01 17:58:58 +1100

    create path, begin prompt

--- server.js
+++ server.js
@@ -3,3 +3,21 @@ const mysql = require('mysql2')
 const express = require('express')
 
 const app = express()
+const PORT = process.env.PORT || 3001
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'R3gg13@home',
+      database: 'courses_db'
+    },
+    console.log(`Connected to the courses_db database.`)
+);
+
+function beginApp() {
+    inquirer
+    .prompt([
+        {}
+    ])
+}
